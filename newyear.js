3b318fff889d9d478e50fa757bdfa7a82eed0de2
Sharmila pannyam <i.sp@leiairship.onmicrosoft.com>
2019-01-04 15:07:20 +0100

    card

---
+++ newyear.js
@@ -0,0 +1,8 @@
+function showText(){
+document.getElementById("showSnowflake").hidden=false;
+document.getElementById("colourbox1").hidden=true;
+document.getElementById("showcard").hidden=false;
+document.getElementById("showSanta").hidden=false;
+document.getElementById("caption").hidden=true;
+
+}
